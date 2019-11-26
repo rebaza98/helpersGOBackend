70e7f752440e70e63cc76f941f132fb5b7054b21
JillROG <rebaza98@gmail.com>
2019-11-26 12:38:46 -0500

    some

--- helpersGOBackend/settings.py
+++ helpersGOBackend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@op+72s=ab*6=vfux=c7b1p09lpfw61ssu*z0#no@#a1h1vrc#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 #ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'helpersgo.herokuapp.com']
 ALLOWED_HOSTS = ['*']
@@ -104,10 +104,27 @@ WSGI_APPLICATION = 'helpersGOBackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fymhdkth',
+        'USER': 'fymhdkth',
+        'PASSWORD': 'nVgQ8LEdlW7HMs4EkurddNgA-TDCtl37',
+        'HOST': 'salt.db.elephantsql.com',
+        'PORT': '',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'betaNegocio',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'Dexter123',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '5432',
     }
 }
 
@@ -163,7 +180,11 @@ USE_TZ = True
 # STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = 'staticfiles'
