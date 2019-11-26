4374413c43c84e86a85bf8ce64f763d5cd97a9a1
JillROG <rebaza98@gmail.com>
2019-11-26 04:46:17 -0500

    j

--- helpersGOBackend/settings.py
+++ helpersGOBackend/settings.py
@@ -156,3 +156,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 #STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
