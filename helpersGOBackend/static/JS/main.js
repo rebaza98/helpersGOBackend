3bfdf4abb324fe5e71085cd9b0ee481269dbe900
JillROG <rebaza98@gmail.com>
2019-11-26 04:28:04 -0500

    10

--- helpersGOBackend/static/JS/main.js
+++
@@ -1,6 +0,0 @@
-const buttonMenu = document.body.querySelector('.js_menu');
-const body = document.body;
-
-buttonMenu.addEventListener('click', function () {
-    body.classList.toggle('show-menu');
-})
