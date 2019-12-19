24ecd49581615bcadf553b8b0366fe1e66e8746e
alexcumpa <53545939+alexcumpa@users.noreply.github.com>
2019-12-19 00:37:13 -0500

    new

--- helpersGOBackend/urls.py
+++ helpersGOBackend/urls.py
@@ -15,11 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from helpersgo import views
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', views.welcome),
+    path('register', views.register),
+    path('login', views.login),
+    path('logout', views.logout),
+
     path('admin/', admin.site.urls),
     path('helpersgo/', include(('helpersgo.urls', 'helpersgo'), namespace='helpersgo')),
 ] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
